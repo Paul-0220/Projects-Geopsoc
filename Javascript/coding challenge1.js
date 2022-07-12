349b3419b717272d4853f8bfc3d8e75121a90201
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-07-12 19:58:03 +0530

    coding challnege

---
+++ Javascript/coding challenge1.js
@@ -0,0 +1,12 @@
+const markMass=78;
+const markHeight=1.69;
+const markBmi=markMass/markHeight**2
+console.log(markBmi)
+
+const johnMass=95;
+const johnHeight=1.88;
+const johnBmi=johnMass/johnHeight**2
+console.log(johnBmi)
+
+const markHigher=markBmi>johnBmi
+console.log(markHigher)
