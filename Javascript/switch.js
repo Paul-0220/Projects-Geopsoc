927484589deba2f1933965d74d44e0ac0a631d52
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-07-13 14:58:30 +0530

    switch case

---
+++ Javascript/switch.js
@@ -0,0 +1,16 @@
+const day="saturday";
+ switch(day){
+    case "monday":
+        console.log('Learning day')
+        break;
+    case "tuesday":
+        console.log("doubt day")
+        break;
+    case "wednesday":
+        console.log("quiz")
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("weekends")
+        break;
+ }
