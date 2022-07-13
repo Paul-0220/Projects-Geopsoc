eea238d208ff62dafb05ea31cd16c5067de908fe
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-07-13 09:27:24 +0530

    type conversion

---
+++ Javascript/types.js
@@ -0,0 +1,10 @@
+// Type conversion........
+let a="23"
+console.log(typeof a)
+let b=Number(a)
+console.log(typeof b)
+console.log(`-----------------------------------------------------------------------`)
+let c=02
+console.log(typeof c)
+let d=String(c)
+console.log(typeof d)
