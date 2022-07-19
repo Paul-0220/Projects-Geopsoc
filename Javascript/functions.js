a14f76406ffad2efe39e0f254f80c52445ad6325
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-07-19 16:51:35 +0530

    functions

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -7,3 +7,37 @@ function fruitProccessor(apple,pineapple){
 
 console.log(fruitProccessor(10,5))
 
+// function declaration and expression
+
+// function declaration
+function printLog(){
+    console.log("logger")
+}
+
+// function expression
+const logg=function(){
+    console.log("logger2")
+}
+
+printLog()
+logg()
+
+
+// Arrow Function
+const arr=(a,b,c)=>a+b+c
+console.log(arr(5,10,20))
+
+// Function calling function
+function cutFruit(fruits){
+    return fruits*4
+}
+
+function fruitJuice(mango,waterMelon){
+    const mangoJuice=cutFruit(mango)
+    const waterJuice=cutFruit(waterMelon)
+
+    const mixJuice=`juice contains ${mango} mango and ${waterMelon} waterMelon`
+    return mixJuice
+}
+
+console.log(fruitJuice(2,3))
