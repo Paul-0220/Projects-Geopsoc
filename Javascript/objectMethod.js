b08d26ab8f2dad63209c0b4205cf466e033b4597
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-07-24 11:17:55 +0530

    objectMethods with this

--- Javascript/objectMethod.js
+++ Javascript/objectMethod.js
@@ -1,16 +1,23 @@
-const bioData={
+const bio={
     fullName:'Prashannjeet Paul',
-    family:['Dad','Mom'],
-    dob:1980,
-    // calcAge: function(){
-    //     // console.log(this)
-    //     return 2040-this.dob
-    // }
-    calcAge: function(){
-        bio.dob=2040-this.age
-        // console.log(this)
-        return this.age;
+    eId:1502,
+    birthYear:1800,
+    calcAge:function(){
+        return 2040-bio.birthYear
     }
 }
+console.log(bio)
+console.log(bio.calcAge())
+console.log(`-----------------------------------------------------------------------------------------------------`)
 
-console.log(bioData.age)
+// used 'this' keyword........
+const bio2={
+    fullName:'Prashannjeet Paul',
+    eId:1502,
+    birthYear:1800,
+    calcAge:function(){
+        return 2040-this.birthYear
+    }
+}
+console.log(bio2)
+console.log(bio2.calcAge())
